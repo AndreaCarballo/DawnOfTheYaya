c0a427b95c8378e048c118f29312c3be5466f535
albertoreyvilela <43855857+albertoreyvilela@users.noreply.github.com>
2018-10-23 19:56:06 +0200

    Add files via upload

---
+++ DebugTexto.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugTexto : MonoBehaviour {
+  
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.F)) //Con F mostramos Debug por pantalla
+        {
+           
+            this.transform.GetComponent<Text>().text = "Objetos"+ GameObject.FindGameObjectsWithTag("Objeto").Length;
+            //Lo que este tageado como Objeto lo cuenta !!
+
+            //GameObject.Find("DebugText").SetActive(!GameObject.Find("DebugText").activeInHierarchy);
+        }
+	}
+}
