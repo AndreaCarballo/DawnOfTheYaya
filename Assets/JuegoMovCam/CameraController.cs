8a35a107af053488dc16ca59b60ce009cb9e0b55
alber <albertoreyvilela@gmail.com>
2018-10-23 18:03:37 +0200

    Anhadido Proyecto Alberto

---
+++ Assets/JuegoMovCam/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform myTarget;
+    public float smoothPositionFactor;
+    public float smoothRotationFactor;
+    // public Transform myPlayer;
+    private Vector3 offsetPosition;
+    // Use this for initialization
+    void Start()
+    {
+        offsetPosition = transform.position - myTarget.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void LateUpdate()
+    {
+        UpdateCameraPosition();
+    }
+
+    private void UpdateCameraPosition()
+    {
+        Vector3 idealPosition;
+
+        idealPosition = myTarget.position + myTarget.rotation * offsetPosition;
+
+        //transform.position = idealPosition;
+        transform.position = Vector3.Lerp(transform.position, idealPosition, Time.deltaTime * smoothPositionFactor);
+
+
+        Quaternion lookRotation = Quaternion.LookRotation(myTarget.forward, myTarget.up);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * smoothRotationFactor);
+
+        // transform.LookAt(myPlayer);
+        // transform.rotation = lookRotation;
+        //transform.forward = myTarget.transform.forward;
+
+
+    }
+}
---
+++ Assets/scripts/NuevaIso.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NuevaIso : MonoBehaviour {
+
+    public GameObject Camara;
+    public GameObject PosicionCamara;
+    Vector3 objetivo;
+    NavMeshAgent nv;
+    // Use this for initialization
+    void Start () {
+        nv = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, 0, 0.1f));
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(new Vector3(0, 0, -0.1f));
+            }
+            else
+            {
+                if (Input.GetKey(KeyCode.D))
+                {
+                    PosicionCamara.transform.SetParent(null);
+                    transform.Rotate(new Vector3(0, 5.0f, 0));
+                    PosicionCamara.transform.SetParent(transform);
+                }
+
+                else
+                {
+                    if (Input.GetKey(KeyCode.A))
+                    {
+                        PosicionCamara.transform.SetParent(null);
+                        transform.Rotate(new Vector3(0, -5.0f, 0));
+                        PosicionCamara.transform.SetParent(transform);
+                    }
+                }
+            }
+
+
+        }Camara.transform.position = Vector3.Lerp(Camara.transform.position, PosicionCamara.transform.position, 0.1f);
+       
+    }
+    
+
+    }
+
