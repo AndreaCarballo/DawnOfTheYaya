dbb6aefb56a190eeba700708c5936407d7d3346a
AndreaCarballo <andrea.ct.750@gmail.com>
2018-11-13 19:03:04 +0100

    Pruebas Q-Learning
    Paquete con dos escenas de prueba que usan los dos scripts para implementar el algoritmo Q-Learning

---
+++ Pruebas Q-Learning/QLearning.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+public class QLearning
+{
+    private List<float[]> QStates;
+    private List<float[]> QActions;
+
+    private int numberPossibleActions; //4: up, down, right, left
+
+    private float[] pastState; //vector that holds the previous state
+    private int pastActionIndex; //index of optimal action (0, 1, 2 or 3)
+    private float[] newState; //vector that holds the current state
+    private float newActionValue; //value of action that will be executed
+
+    private float learningRate = 0.8f;
+    private float discountFactor = 0.95f;
+    private float e = 1; // Initial epsilon value for random action selection.
+    private float eMin = 0.1f; // Lower limit for epsilon.
+    private int eSteps = 2000; // Number of steps to lower e to eMin.
+
+    private bool firstIteration;
+    System.Random random = new System.Random();
+
+    public QLearning(int possActs)
+    {
+        QStates = new List<float[]>();
+        QActions = new List<float[]>();
+        numberPossibleActions = possActs;
+
+        firstIteration = true;
+    }
+
+    public int getAction(float[] currentState, float reward)
+    {
+        iterationUpdate(currentState, reward); //updates the action quality
+
+
+        pastState = currentState; //saves the current state as the previous one
+
+        firstIteration = false;
+
+        int actionIndex = random.Next(0, numberPossibleActions); //if it's the first iteration the action is randomly generated
+
+        bool exists = searchState(currentState, false); //check if current state is stored
+        if (exists)
+        {
+            //policy e-greedy
+            if (Random.Range(0f, 1f) < e)
+            {
+                pastActionIndex = actionIndex; //we use the randomly generated action
+            }
+            if (e > eMin)
+            {
+                e = e - ((1f - eMin) / (float)eSteps);
+            }
+            return pastActionIndex; //returns the action the agent will take
+
+        } else //if current state is not stored
+        {
+            float[] actionVals = new float[numberPossibleActions];
+            for (int i = 0; i < numberPossibleActions; i++)
+            {
+                actionVals[i] = 0f; //actions associated with this state have blank values
+            }
+            QStates.Add(pastState);
+            QActions.Add(actionVals);
+        }
+
+        pastActionIndex = actionIndex; //we use the randomly generated action
+        return pastActionIndex;
+    }
+
+    public void iterationUpdate(float[] nState, float reward)
+    {
+        if (!firstIteration) //only if it's not the first iteration
+        {
+            newState = nState;
+
+            bool exists = searchState(newState, true); //checks if the new state is stored
+
+            for (int i = 0; i < QStates.Count; i++)
+            {
+                float[] state = QStates.ElementAt(i);
+                float[] actions = QActions.ElementAt(i);
+
+                if (state[0] == pastState[0] && state[1] == pastState[1]) //we update the quality of the past action taken for the previous state
+                {
+
+                    if (exists) //if the new state is already stored we take into account its optimal action value
+                    {
+                        actions[pastActionIndex] = (actions[pastActionIndex] + learningRate * (reward + discountFactor * newActionValue - actions[pastActionIndex]));
+                    }
+
+                    if (!exists) //if it isn't, we don't
+                    {
+                        actions[pastActionIndex] = (actions[pastActionIndex] + learningRate * (reward - actions[pastActionIndex]));
+                    }
+                }
+            }
+        }
+    }
+
+    public bool searchState(float[] stateToFind, bool update)
+    {
+        bool exists = false;
+        if (QStates.Count > 0) //checks if q states has been initialized
+        {
+
+            for (int i = 0; i < QStates.Count; i++)
+            {
+                // saves every state vector with its corresponding action vector
+                float[] state = QStates.ElementAt(i);
+                float[] actions = QActions.ElementAt(i);
+
+                if (state[0] == stateToFind[0] && state[1] == stateToFind[1])
+                {
+                    exists = true; //marks that the state is already stored
+
+                    if (!update) //getting optimal action to send to agent
+                    {
+                        pastActionIndex = Array.IndexOf(actions, actions.Max()); //saves index of optimal action
+                       //Debug.Log("Picked action value:          " + actions[pastActionIndex]); //prints the value associated with the action
+                        
+                    } else //getting quality of action for the formula
+                    {
+                        newActionValue = actions.Max(); //saves value of optimal action
+                    }
+                    return exists;
+                }
+            }
+        }
+        return exists;
+    }
+}
+
---
+++ Pruebas Q-Learning/ZombieAgent.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieAgent : MonoBehaviour {
+
+    public GameObject player;
+    public float walkingDistance = 1f; //speed
+    public float xStartPosition = 0f;
+    public float yStartPosition = 0.5f;
+    public float zStartPosition = 4f;
+
+    //position to restart the player for training
+    public int xPlayer = 4;
+    public int zPlayer = 4;
+
+    //variables that save the previous player position
+    private float lastDistance = 0f;
+
+
+    Rigidbody rb;
+    //private NavMeshAgent myNavMeshAgent;
+
+    private bool positiveTrigger = false;
+    private bool negativeTrigger = false;
+    private bool restartPostion = false;
+
+    QLearning QL = new QLearning(4); //4 possible actions
+
+    public float positiveReward = 10f; //reward when it hits the player
+
+    //uncomment when an object produces a negative reward
+    //public float negativeReward = -10f;
+
+    public float timePunishment = -1f; //punishment for each update
+    public float distanceReward = 1f; //reward when it gets close
+
+    private float currentReward = 0f;
+
+    System.Random random = new System.Random();
+
+    // Use this for initialization
+    void Start () {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //The state of the world is the relative position of the zombie to the player
+        Vector3 relativePositionToPlayer = player.transform.InverseTransformPoint(transform.position);
+        relativePositionToPlayer.x = (float)Math.Round((double)relativePositionToPlayer.x);
+        relativePositionToPlayer.z = (float)Math.Round((double)relativePositionToPlayer.z);
+        float[] qState = new float[] { relativePositionToPlayer.x, relativePositionToPlayer.z };
+
+        //Debug.Log("qState  " + qState[0] + ", " + qState[1]);
+
+        int action = QL.getAction(qState, currentReward); //gets the optimal action from q-learning
+        currentReward = 0f; //reinitializes reward
+
+        Move(action); //moves the zombie
+
+        currentReward += timePunishment; //punishment for step
+
+        if (positiveTrigger) //checks if it hit the player
+        {
+            currentReward += positiveReward;
+        }
+
+        //uncomment when there's an object that produces negative rewards
+        //if (negativeTrigger)
+        //{
+        //    currentReward += negativeReward;
+        //}
+
+        
+        float currentDistance = (float)Math.Round((double)Vector3.Distance(transform.position, player.transform.position));
+        Debug.Log("before " + lastDistance + " after: " + currentDistance);
+
+        if (currentDistance < lastDistance) //if the agent got closer to the player it'll get a reward
+        {
+            Debug.Log("closer");
+            currentReward += distanceReward;
+        }
+
+        lastDistance = currentDistance;
+        rb.velocity = Vector3.zero;
+
+        if (restartPostion) //for training
+        {
+            transform.position = new Vector3(xStartPosition, yStartPosition, zStartPosition);
+            player.transform.position = new Vector3(random.Next(-xPlayer, xPlayer), yStartPosition, random.Next(-zPlayer, zPlayer));
+            restartPostion = false;
+            //Debug.Log("restarting position");
+        }
+    }
+
+    // detect collision with object
+    void OnTriggerEnter(Collider col)
+    {
+
+        // reaction to positive reward
+        if (col.gameObject.name == "Player")
+        {
+            positiveTrigger = true;
+            restartPostion = true;
+        }
+
+
+        //uncomment when there's an object that produces negative rewards
+        // reaction to negative reward
+        //if (col.gameObject.name == "")
+        //{
+        //    negativeTrigger = true;
+        //}
+
+    }
+
+    // detect collision with object
+    void OnTriggerExit(Collider col)
+    {
+        // reaction to positive reward
+        if (col.gameObject.name == "Player")
+        {
+            positiveTrigger = false;
+        }
+
+
+        //uncomment when there's an object that produces negative rewards
+        // reaction to negative reward
+        //if (col.gameObject.name == "")
+        //{
+        //    negativeTrigger = false;
+        //}
+    }
+
+    void Move(int inputVal)
+    {
+        if (inputVal == 0) //up
+        {
+            rb.MovePosition(transform.position + transform.forward * walkingDistance);
+        }
+        if (inputVal == 1) //down
+        {
+            rb.MovePosition(transform.position + transform.forward * -walkingDistance);
+        }
+        if (inputVal == 2) //right
+        {
+            rb.MovePosition(transform.position + transform.right * walkingDistance);
+        }
+        if (inputVal == 3) //left
+        {
+            rb.MovePosition(transform.position + transform.right * -walkingDistance);
+        }
+        
+    }
+}
