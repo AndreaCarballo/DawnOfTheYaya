faaf9eddb4ee1acb884e9eb66662ef706277097a
AndreaCarballo <andrea.ct.750@gmail.com>
2019-01-24 20:26:27 +0100

    Última versión

---
+++ _Dawn Of The Yaya/Assets/_Scripts/Inventory/ObjectInteract.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ObjectInteract : MonoBehaviour
+{
+
+    #region Variables
+    //Variables
+    private Color firstColor;
+    private CursorMode cursorMode = CursorMode.ForceSoftware;
+    private Vector2 hotSpot = Vector2.zero;
+    private GameObject playerObject;
+    private bool activeInteract = false;
+    private float prevSpeed;
+    private GameObject lifeHUD;
+    private GameObject mainCamera;
+    private bool playSound;
+    private Animator anim;
+
+    //Visible Variables
+    public Texture2D cursorTextureHand;
+    public Texture2D cursorTexture;
+    public Inventory inventory;
+    public GameObject endPreAlphaMenu;
+    public AudioClip pickClip;
+    public GameObject taxiEndMusic;
+    public GameObject ambientSound;
+    public GameObject nigthSound;
+    public GameObject invent;
+    #endregion
+
+
+    // Use this for initialization
+    void Start()
+    {
+        firstColor = gameObject.GetComponent<Renderer>().material.color;
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+        prevSpeed = playerObject.GetComponent<NavMeshAgent>().speed;
+        lifeHUD = GameObject.Find("HealthUI");
+        invent = GameObject.Find("Inventory");
+        mainCamera = GameObject.Find("MainCamera");
+        playSound = false;
+        anim = playerObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(playerObject.transform.position, transform.position) <= 2 && activeInteract)
+        {
+            switch (gameObject.name)
+            {
+                case "Apple":
+                    inventory.AddItemByID(0);
+                    playSound = true;
+                    anim.SetTrigger("PickObject");
+                    break;
+                case "Brocoli":
+                    inventory.AddItemByID(1);
+                    playSound = true;
+                    anim.SetTrigger("PickObject");
+                    break;
+                case "Bananas":
+                    inventory.AddItemByID(2);
+                    playSound = true;
+                    anim.SetTrigger("PickObject");
+                    break;
+                case "RoastedFruits":
+                    inventory.AddItemByID(3);
+                    playSound = true;
+                    anim.SetTrigger("PickObject");
+                    break;
+                case "Taxi":
+                    //End of PreAlpha
+                    playerObject.SetActive(false);
+                    //ambientSound.SetActive(false);
+                    //taxiEndMusic.SetActive(true);
+                    //endPreAlphaMenu.SetActive(true);
+                    //lifeHUD.SetActive(false);
+                    break;
+                case "FINAL":
+                    //End Level 1
+                    //playerObject.SetActive(false);
+                    ambientSound.SetActive(false);
+                    taxiEndMusic.SetActive(true);
+                    endPreAlphaMenu.SetActive(true);
+                    nigthSound.SetActive(false);
+                    invent.SetActive(false);
+                    //lifeHUD.SetActive(false);
+                    break;
+                case "Bottle":
+                    inventory.AddItemByID(4);
+                    playSound = true;
+                    anim.SetTrigger("PickObject");
+                    break;
+                case "Sneaker":
+                    inventory.AddItemByID(5);
+                    playSound = true;
+                    anim.SetTrigger("PickObject");
+                    break;
+                case "Rock":
+                    inventory.AddItemByID(6);
+                    playSound = true;
+                    anim.SetTrigger("PickObject");
+                    break;
+                case "Cane":
+                    inventory.AddItemByID(7);
+                    playSound = true;
+                    anim.SetTrigger("PickObject");
+                    break;
+            }
+
+            activeInteract = false;
+            Cursor.SetCursor(cursorTexture, Vector2.zero, cursorMode);
+            if (playSound)
+                PlaySound();
+            if (gameObject.name != "Taxi")
+                Destroy(gameObject);
+        }
+    }
+
+    #region Methods
+
+    void PlaySound()
+    {
+        playerObject.GetComponent<AudioSource>().PlayOneShot(pickClip, 0.6f);
+        playSound = false;
+    }
+
+    void OnMouseOver()
+    {
+        //Linearly interpolates between colors a and b by time.
+        gameObject.GetComponent<Renderer>().material.color = Color.Lerp(firstColor,
+            Color.yellow,
+            Mathf.PingPong(Time.time, 1));
+
+        //Change the cursor
+        Cursor.SetCursor(cursorTextureHand, new Vector2(hotSpot.x + 10f, hotSpot.y - 1f), cursorMode);
+
+        //Player movement if we press "left button mouse"
+        if (Input.GetMouseButtonDown(0))
+        {
+            playerObject.GetComponent<MovClick>().SetTargetPositionAndMove();
+            activeInteract = true;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            playerObject.GetComponent<NavMeshAgent>().speed = 0f;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            playerObject.GetComponent<NavMeshAgent>().speed = prevSpeed;
+            playerObject.GetComponent<NavMeshAgent>().SetPath(new NavMeshPath());
+        }
+    }
+
+    void OnMouseExit()
+    {
+        gameObject.GetComponent<Renderer>().material.color = firstColor;
+        Cursor.SetCursor(cursorTexture, Vector2.zero, cursorMode);
+    }
+    #endregion
+}
---
+++ _Dawn Of The Yaya/Assets/_Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int Healthstart =100;
+    public int currentHealth;
+    public int cantidadRestaportiempo = 0;
+    public Slider healthSlider;
+    public Image damageImage;
+    //public AudioClip death;
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+    public GameObject MenuGameOver;
+    public GameObject gato;
+    bool isDead;
+    bool damage;
+    
+    public GameObject gameOverSound;
+    public GameObject ambientSound;
+    public GameObject inventory;
+
+    [HideInInspector]
+    public int lifeLost = 0;
+
+    private void Start()
+    {
+        inventory = GameObject.Find("Inventory");
+    }
+
+    private void Awake()
+    {
+
+        currentHealth = Healthstart; //inicianmos la vida a 100;
+
+    }
+    private void Update()
+    {
+
+
+    }
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount; //decrementamos salud en la cantidad
+        lifeLost += amount;
+        healthSlider.value = currentHealth; //Actualizamos slider de vida
+        if (currentHealth <= 0 && !isDead) {
+            Death();
+        }
+
+    }
+    public void TakeLife(int amount)
+    {
+        currentHealth += amount; //decrementamos salud en la cantidad
+        healthSlider.value = currentHealth; //Actualizamos slider de vida
+        if (currentHealth >= Healthstart)
+        {
+            currentHealth = Healthstart;
+        }
+
+    }
+
+    void Death()
+    {
+        isDead = true; //jugador muerto
+        //FindObjectOfType<AudioManagerGame>().Play("death");
+        
+        ambientSound.SetActive(false);
+        gameOverSound.SetActive(true);
+        MenuGameOver.SetActive(true);
+        inventory.SetActive(false);
+        //Destroy(this.gameObject, 0f);
+        //Destroy(gato.gameObject, 0f);
+    }
+
+
+}
